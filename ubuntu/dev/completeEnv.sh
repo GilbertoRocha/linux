274b3b1cf9a7770c39ab015dad49dba88853536a
Gilberto <giba.darocha@gmail.com>
2026-01-28 07:15:21 -0300

    Added Ubuntu dotnet.sh install and completeEnv.sh install

---
+++ ubuntu/dev/completeEnv.sh
@@ -0,0 +1,6 @@
+sudo chmod +x docker.sh
+./docker.sh
+sudo chmod +x dotnetcore.sh
+./dotnetcore.sh
+sudo chmod +x K3d.sh
+./K3d.sh
---
+++ ubuntu/dev/dotnetcore.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install -y dotnet-sdk-10.0
+sudo apt install -y aspnetcore-runtime-10.0
